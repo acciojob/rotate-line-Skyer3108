c40b36558187926ad52b3f6520101d25538207fd
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 16:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let angle = 0;
+
+function rotateLine() {
+    angle = (angle + 2) % 360;
+	document.getElementById('line').style.transform = `rotate(${angle}deg)`;
+}
+
+
+setInterval(rotateLine, 20);
